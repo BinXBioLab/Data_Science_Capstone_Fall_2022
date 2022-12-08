8d7b1d33e027984f73c5f9b4be338209c162ce9c
Darvesh Gorhe <dgorhe123@gmail.com>
2022-12-08 09:49:30 -0500

    Tested pipeline through post-LIGER and rank genes

--- optimized_codes/scripts/pipeline.py
+++ optimized_codes/scripts/pipeline.py
@@ -1,71 +1,76 @@
+from __future__ import annotations
+import sys
 import pickle
-import gc
 import scanpy as sc
 import git
 import pathlib
 import os
-import sys
 import matplotlib.pyplot as plt
-import numpy as np
-import os
 import pandas as pd
 import seaborn as sns
-from helper import write_anndata
+from helper import write_anndata, create_color_palette
 import wget
 from scipy.sparse import csr_matrix
 from tqdm import tqdm
 import gseapy
+import click
+from joblib import parallel_backend
+
 
 git_repo = git.Repo(".", search_parent_directories=True)
 git_root = git_repo.git.rev_parse("--show-toplevel")
 
 topDir = os.path.join(git_root, "raw_data/Pasca_scRNAseq/")
 prefix = 'pasca'
-inDir = pathlib.Path(topDir,"raw")
 outDir = pathlib.Path(topDir,"outputs")
 interDir = pathlib.Path(topDir,"inter-test")
 
 sns.set_style('white')
 plt.rcParams['savefig.facecolor'] = 'w'
 sc.settings.verbosity = 3             # verbosity: errors (0), warnings (1), info (2), hints (3)
-sc.set_figure_params(scanpy=True, dpi=120, dpi_save=150, frameon=True, vector_friendly=True, fontsize=14) 
-sc.settings.figdir = outDir('fig_supp')
-
-# TODO: Check later if this can be removed
-samples = ['pasca']
-prefix = samples[0]
-prefix
+sc.set_figure_params(
+    scanpy=True, 
+    dpi=120, 
+    dpi_save=150, 
+    frameon=True, 
+    vector_friendly=True, 
+    fontsize=14
+)
+sc.settings.figdir = os.path.join(outDir, 'fig_supp')
+sc.settings.n_jobs = 4
 
 def generate_figures(anndata: sc.AnnData) -> None:
     # Compute neighborhood graph
     # use_rep means use X_liger result to represent X for calculation
     # other adjustable parameters that control uMAP output are n_neighbors, metrics(distance), min_dist
-    sc.pp.neighbors(
-        anndata,
-        n_neighbors=15,
-        random_state=42,
-        use_rep= 'X_liger',
-    )
+    with parallel_backend('threading', n_jobs=4):
+        sc.pp.neighbors(
+            anndata,
+            n_neighbors=15,
+            random_state=42,
+            use_rep= 'X_liger'
+        )
 
     # conduct umap
-    sc.tl.umap(
-        anndata, 
-        random_state=42, 
-        spread=3.0, 
-        min_dist=0.1
-    )
+    with parallel_backend('threading', n_jobs=4):
+        sc.tl.umap(
+            anndata,
+            random_state=42,
+            spread=3.0,
+            min_dist=0.1
+        )
 
     # Plot UMAP
-    sc.pl.umap(anndata, color=["PAX6"], show=False) 
+    sc.pl.umap(anndata, color=["PAX6"], show=False)
     sc.tl.leiden(anndata)
     sc.pl.umap(anndata, color=['leiden'], show=False)
 
     return anndata
 
-def rank_genes_and_save(anndata: sc.AnnData) -> None:
-    sc.tl.rank_genes_groups(adata, 'leiden', method='wilcoxon')
-    sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False)
-    write_anndata(anndata, interDir, "pasca_log1p_liger.h5ad")
+def rank_genes_and_save(adata: sc.AnnData) -> None:
+    sc.tl.rank_genes_groups(adata, 'leiden', method='wilcoxon', show=False)
+    sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False, show=False)
+    write_anndata(adata, interDir, "pasca_log1p_liger.h5ad")
 
 def download_and_process_reference(
     counts_url=None, 
@@ -245,7 +250,7 @@ def rank_gene_analysis(adata: sc.AnnData, gene='SETD1A', count_min=1) -> sc.AnnD
 
     return adata
 
-def differential_expression_analysis(adata):
+def differential_expression_analysis(adata, cutoff=0, gene = 'SETD1A'):
     # backup new X data
     adata.layers['bck_X'] = adata.X
 
@@ -261,11 +266,9 @@ def differential_expression_analysis(adata):
     adata_test = adata[adata.obs['nowakowski'].isin(cellType)]
     adata_test.obs['n_genes'] = (adata_test.X > 0).sum(1) # recompute number of genes expressed per cell
 
-    # get the cells with high targetGene expression X > mycut
-    mycut = 0
-    mygene = 'SETD1A'
+    # get the cells with high targetGene expression X > cutoff
     # generate a filter in the obs group
-    adata_test.obs['targetGene'] = (adata_test[:,[mygene]].X > mycut).sum(1) 
+    adata_test.obs['targetGene'] = (adata_test[:,[gene]].X > cutoff).sum(1) 
 
     adata_test2 = adata_test[adata_test.obs['targetGene'] >= 1]
     
@@ -288,11 +291,15 @@ def differential_expression_analysis(adata):
     gene_set_names = gseapy.get_library_name(database='Human')
     res = sc.get.rank_genes_groups_df(adata_test2, group=['CT','FS','SP'], key='wilcoxon',pval_cutoff=0.1)
     res.to_parquet(os.path.join(interDir, f'{prefix}.DEG_rank_genes.parquet'))
-    
 
+    run_mast(adata_test)
+    mast_postprocessing
+    
 def run_mast(
+    adata: sc.AnnData,
     cell_types=['EN-PFC1','EN-PFC2','EN-PFC3','EN-V1-1','EN-V1-2','EN-V1-3']
     ) -> None:
+    
     # subset the adata for the cell type we are interested in
     adata_test = adata[adata.obs['nowakowski'].isin(cell_types)]
     adata_test.obs['n_genes'] = (adata_test.X > 0).sum(1) # recompute number of genes expressed per cell
@@ -302,23 +309,125 @@ def run_mast(
 
     # produce mast metadata csv
     keys = ['n_genes', 'condition', 'batch'] #, ... additional covariates present in obs slot of adata that you may want to use ...  ]
-    adata_test.obs[keys].to_csv(os.path.join(interDir, 'nowakowski_EN.mast_metadata.csv'))
+    adata_path = os.path.join(interDir, 'nowakowski_EN.mast_metadata.csv')
+    adata_test.obs[keys].to_csv(adata_path)
     
-    # TODO: Figure out best way to actually run MAST locally
-    # os.command('R MAST_analaysis_BX.R <argument 1> ... <argument n>')
+    # if script is available, run MAST_analysis_BX.R
+    script_path = os.path.join(git_root, 'optimized_codes', 'R', 'MAST_analysis_BX.R')
+    if os.path.isfile(script_path):
+        os.system('Rscript --vanilla ' + script_path + ' ' + adata_path)
+    else:
+        click.echo("MAST_analysis_BX.R not found here: " + script_path)        
+        click.echo("Falling back to using rpy2")
 
+        import rpy2.robjects as robjects
+        from rpy2.robjects import pandas2ri
+        pandas2ri.activate()
+        robjects.r(
+            '''
+            <script body>
+            '''
+        )
 
-if __name__ == "__main__":
+    ent_de = pd.read_csv(os.path.join(interDir, 'nowakowski_EN.mast_ent_de.csv'))
+    return ent_de
+
+def mast_postprocessing(ent_de) -> None:
+    ent_de.index = ent_de.primerid
+
+    ## import annotation from the biomart database
+    annot = sc.queries.biomart_annotations(
+                "hsapiens",
+                ['hgnc_symbol',"chromosome_name","start_position", "end_position"],
+            ).set_index("hgnc_symbol")
+
+    ent_de_ann = ent_de.merge(annot, left_index=True, right_index=True)
+    ent_de_ann.sort_values(by=['FDR'], inplace=True)
+
+    filename = f"{prefix}.log1p_liger_singleR_nowakowski_3cellTypes_MAST.parquet "
+    output_path = os.path.join(interDir, filename)
+    ent_de_ann.to_parquet(output_path)
+
+def find_highly_variable_genes(adata):
+    ad = adata # remove this later
+    sc.pp.highly_variable_genes(ad, min_mean= 0.2,max_mean=10, min_disp=0.1)
+    sc.pl.highly_variable_genes(ad)
+
+    # form a new object with highly variable genes only. Assume these genes are the key genes that can explain various differences in the data
+    ad_hv = ad[:, ad.var.highly_variable]
+    filename = "cntnap2-s26_norm_log_hv.h5ad"
+    output_path = os.path.join(interDir, filename)
+    ad_hv.write(output_path)
+
+def visualize_highly_variable_genes(adata):
+    ad_hv = adata # remove this later
+
+    # adjust or readjust the pca and umap
+    sc.tl.pca(ad_hv, svd_solver='arpack')
+    sc.pp.neighbors(
+        ad_hv,random_state=42,
+        use_rep= 'X_liger_k25'
+    )
+    sc.tl.umap(ad_hv,spread=3)
+
+    # TODO: Make this more general/modular 
+    sc.pl.umap(
+        ad_hv, 
+        color=['CNTNAP2','PAX6','NEUROD6','NEUROD2'], 
+        palette='hsv', 
+        legend_fontsize=8, 
+        legend_loc="on data"
+    )
+
+    # Format later
+    sc.pl.umap(ad_hv[ad_hv.obs['condition'] =='WT',:],color=['CNTNAP2','PAX6','NEUROD6','NEUROD2'], palette='hsv', legend_fontsize=6,title='WT : Leiden', legend_loc="on data")
+    sc.pl.umap(ad_hv[ad_hv.obs['condition'] =='MUT',:],color=['CNTNAP2','PAX6','NEUROD6','NEUROD2'], palette='hsv', legend_fontsize=6,title='MUT : Leiden',legend_loc="on data")
+
+    filename = "cntnap2-s26_norm_log_hv_leiden.h5ad"
+    output_path = os.path.join(interDir, filename)
+    ad_hv.write(output_path)
+
+def human_pfc_data(
+    outfile: str | pathlib.Path, 
+    exprMatrix:str|None = None, 
+    meta:str|None = None) -> sc.AnnData:
+    
+    # Download reference material
+    if exprMatrix == None:
+        matrix_filepath = os.path.join(interDir, "exprMatrix.tsv.gz")
+        wget.download("https://cells.ucsc.edu/cortex-dev/exprMatrix.tsv.gz", matrix_filepath)
+
+    if meta == None:
+        meta_filepath = os.path.join(interDir, "meta.tsv")
+        wget.download("https://cells.ucsc.edu/cortex-dev/meta.tsv", meta_filepath)
+    
+    meta = pd.read_csv(meta_filepath, sep='\t')
+    exprMatrix = sc.read_text(exprMatrix)
+
+    h_ad = exprMatrix.transpose()
+    h_ad.obs = meta
+    output_path = os.path.join(interDir, outfile)
+    h_ad.write(output_path)
+
+    return h_ad
+
+@click.command()
+@click.option('--input', '-i', type=click.Path(exists=True), default=None, help='Liger output file')
+def run():
     # Read files
     path_to_anndata = os.path.join(interDir, "pasca_log1p.h5ad")
     path_to_liger = os.path.join(interDir, "pasca.liger.csv")
+    
     df_liger = pd.read_csv(path_to_liger, index_col=0)
     adata = sc.read(path_to_anndata)
 
     # Generating figures based on Liger output
     adata.obsm['X_liger'] = df_liger.loc[adata.obs_names, :].values
     adata = generate_figures(adata)
+    
+    adata.uns['log1p']["base"] = None # To avoid KeyError: 'base' with rank_gene_groups()
     rank_genes_and_save(adata)
+    sys.exit(0)
 
     # ???
     postqc_path = os.path.join(interDir, "pasca_postqc.h5ad")
@@ -392,11 +501,9 @@ if __name__ == "__main__":
     ## 7f END ##
 
     ## Part 6 START ##
-    
     adata.write(os.path.join(interDir, "pasca.log1p_liger_med_singleR_noglyc.h5ad"))
-    # write metadata to separate file earmarked for CellPhoneDB
-    # write counts to separate file earmarked for CellPhoneDB
-    
+    # *** write metadata to separate file earmarked for CellPhoneDB ***
+    # *** write counts to separate file earmarked for CellPhoneDB ***
     ## Part 6 END ##
 
     ## Part 7 START ##
@@ -410,3 +517,11 @@ if __name__ == "__main__":
     ## Part 9 START ##
     adata = rank_gene_analysis(adata)
     ## Part 9 END ##
+    
+    ## Part 13 START ##
+    adata = differential_expression_analysis(adata)
+    ## Part 13 END ##
+
+
+if __name__ == "__main__":
+    run()
