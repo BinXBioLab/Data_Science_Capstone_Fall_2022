d4144c273d8c4a2fcce7b8267f64ff8c40ea4e32
Darvesh Gorhe <dgorhe123@gmail.com>
2022-12-24 17:29:22 -0500

    Removed unncessary lines of code and added TODO notes for areas of improvement

--- tangram/scripts/Tangram_CPU.py
+++ tangram/scripts/Tangram_CPU.py
@@ -1,42 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-from google.colab import drive
-drive.mount('/content/drive')
-get_ipython().system('mkdir /content/MyDrive')
-get_ipython().system('mount --bind /content/drive/My\\ Drive /content/MyDrive')
-# the magic % will help with the cd command :)
-get_ipython().run_line_magic('cd', '/content')
-
-
-# In[ ]:
-
-
-get_ipython().system('pip install scanpy')
-get_ipython().system('pip install squidpy')
-get_ipython().system('pip install scikit-image')
-get_ipython().system('pip install tangram-sc')
-get_ipython().system('pip install gdown')
-# to upgrade
-get_ipython().system('pip install --upgrade gdown')
-
-
-# In[1]:
-
-
+import gc
 import scanpy as sc
 import squidpy as sq
 import numpy as np
 import pandas as pd
-from anndata import AnnData
-import pathlib
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import skimage
-import seaborn as sns
 import tangram as tg
 
 
@@ -50,9 +19,6 @@ import tangram as tg
 # 
 # 3. `ad_map`: The spatial mapping file. It is a result of the previous notebook `Tangram_GPU.ipynb`.
 
-# In[2]:
-
-
 adata_sc = sc.read_h5ad("pasca.log1p_liger_med_singleR_noglyc.h5ad")
 adata_st = sc.read_h5ad("adata_st_postproj.h5ad")
 ad_map = sc.read_h5ad("ad_map.h5ad")
@@ -62,31 +28,14 @@ ad_map = sc.read_h5ad("ad_map.h5ad")
 # # 2. Analyzing data
 
 # ## 2.1 Visualizing initial data 
-
-# In[3]:
-
-
 img = sq.datasets.visium_fluo_image_crop()
-
-
-# In[4]:
-
-
 sc.pl.umap(
     adata_st, color=["clusters"], palette=sc.pl.palettes.default_20
 )
 
-
-# In[5]:
-
-
 clusters_colors = dict(
     zip([str(i) for i in range(18)], adata_st.uns["clusters_colors"])
 )
-
-
-# In[6]:
-
 
 plt.rcParams["figure.figsize"] = (8, 8)
 ad = adata_st.copy()
@@ -106,10 +55,6 @@ sc.pl.spatial(
 
 plt.tight_layout()
 
-
-# In[7]:
-
-
 fig, axs = plt.subplots(1, 2, figsize=(20, 5))
 sc.pl.spatial(
     adata_st, color="clusters", alpha=0.7, frameon=False, show=False, ax=axs[0]
@@ -123,10 +68,6 @@ plt.tight_layout()
 # ## 2.2 Pre-process AnnDatas
 
 # This preprocessing is required for `adata_sc` to be in the correct format for further experiments.
-
-# In[8]:
-
-
 adata_sc.uns['log1p']["base"] = None
 sc.tl.rank_genes_groups(adata_sc, groupby="nowakowski_med", use_raw=False)
 markers_df = pd.DataFrame(adata_sc.uns["rank_genes_groups"]["names"]).iloc[0:100, :]
@@ -134,93 +75,38 @@ markers = list(np.unique(markers_df.melt().value.values))
 len(markers)
 tg.pp_adatas(adata_sc, adata_st, genes=markers)
 
-
-# In[9]:
-
-
 # alternative 2: saving the experiment result from the previous gpu notebook can be used but pp_adatas don't take much time.
 # It is not worth generating another intermediate file.
 #adata_sc = sc.read_h5ad("adata_sc.h5ad")
 
 
 # ## 2.3 Visualize cell type maps
-
-# In[10]:
-
-
 tg.project_cell_annotations(ad_map, adata_st, annotation="nowakowski_med")
 annotation_list = list(pd.unique(adata_sc.obs['nowakowski_med']))
 tg.plot_cell_annotation_sc(adata_st, annotation_list)
-
-
-# In[11]:
-
-
 #separation of two samples visualize.
-
-
-# In[12]:
-
 
 tg.plot_training_scores(ad_map, bins=20, alpha=.5)
 
 
 # ## 2.4 Generate new spatial data via aligned single cells
-
-# In[13]:
-
-
 # This part to go to CPU.
 ad_ge = tg.project_genes(adata_map=ad_map, adata_sc=adata_sc)
 #ad_ge = sc.read_h5ad("ad_ge.h5ad")
 
-
-# In[14]:
-
-
-import gc
 gc.collect()
 
-
-# In[15]:
-
-
 df_all_genes = tg.compare_spatial_geneexp(ad_ge, adata_st, adata_sc)
-df_all_genes
-
-
-# In[16]:
-
 
 tg.plot_auc(df_all_genes)
 
 
 # ### Inspect predictions
-
-# In[17]:
-
-
 # we have to use low letter
 GOI=['pax6','sox2','foxg1','neurod2','prodh','ranbp1','crkl','hira']
-
-
-# In[ ]:
-
-
 tg.plot_genes_sc(GOI, adata_measured=adata_st, adata_predicted=ad_ge, perc=0.02)
 
-
-# In[49]:
-
-
-adata_st
-
-
 # two kinds of data, one is the patient the other is control and if we can predicted(control) predicted(patient).
-
-# In[18]:
-
-
 plt.rcParams["figure.figsize"] = (20, 20)
 ad = ad_ge.copy()
 sc.pl.spatial(
@@ -241,28 +127,6 @@ plt.tight_layout()
 
 
 # ## 2.? Try to compare two groups
-
-# In[19]:
-
-
-adata_st
-
-
-# In[20]:
-
-
-adata_sc
-
-
-# In[21]:
-
-
-ad_map
-
-
-# In[22]:
-
-
 GOI=['pax6','sox2','foxg1','neurod2','prodh','ranbp1','crkl','hira']
 sc.pl.spatial(
             ad_ge,
@@ -271,17 +135,9 @@ sc.pl.spatial(
 
 
 # ## 2.5 Deconvolution via alignment
-
-# In[23]:
-
-
 sf = adata_st.uns['spatial']['V1_Human_Brain_Section_1']["scalefactors"]["tissue_hires_scalef"]
 img = sq.im.ImageContainer(adata_st.uns['spatial']['V1_Human_Brain_Section_1']['images']['hires'],
                            scale = sf, library_id = 'V1_Human_Brain_Section_1')
-
-
-# In[24]:
-
 
 sq.im.process(img=img, layer="image", method="smooth")
 sq.im.segment(                                                                                
@@ -290,10 +146,6 @@ sq.im.segment(
     method="watershed",
     channel=0,
 )
-
-
-# In[25]:
-
 
 inset_y = 1500
 inset_x = 1700
@@ -343,10 +195,6 @@ axs[2].set_xticks([])
 axs[2].set_yticks([])
 axs[2].set_title("Nucleous segmentation", fontdict={"fontsize": 20});
 
-
-# In[26]:
-
-
 features_kwargs = {
     "segmentation": {
         "label_layer": "segmented_watershed",
@@ -366,70 +214,19 @@ sq.im.calculate_image_features(
     mask_circle=True,
 )
 
-
-# In[27]:
-
-
 adata_st.obs["cell_count"] = adata_st.obsm["image_features"]["segmentation_label"]
 sc.pl.spatial(adata_st, color=["cell_count"], frameon=False)
-
-
-# In[28]:
-
-
-adata_st.obsm['image_features']
-
-
-# In[34]:
-
 
 ad = adata_st.copy()
 print(ad.var_names)
 ad.obs.columns
 #sq.pl.spatial_scatter(ad, color=["nowakowski_med"])
 
-
-# In[35]:
-
-
-ad
-
-
-# In[37]:
-
-
 tg.create_segment_cell_df(ad)
 ad.uns["tangram_cell_segmentation"].head()
 
-
-# In[38]:
-
-
 ad.obs = pd.concat([adata_st.obs, adata_st.obsm["tangram_ct_pred"]], axis=1)
 annotation_list = ad_map.obs['nowakowski_med']
-annotation_list
-
-
-# In[44]:
-
-
-ad
-
-
-# In[45]:
-
-
-ad_map
-
-
-# In[46]:
-
-
-adata_sc
-
-
-# In[75]:
-
 
 xs = ad.obsm["spatial"][:, 1]
 ys = ad.obsm["spatial"][:, 0]
@@ -445,10 +242,6 @@ df_vox_cells = df_vox_cells = pd.DataFrame(
 )
 df_vox_cells
 
-
-# In[60]:
-
-
 def one_hot_encoding(l, keep_aggregate=False):
     df_enriched = pd.DataFrame({"cl": l})
     for i in l.unique():
@@ -457,10 +250,8 @@ def one_hot_encoding(l, keep_aggregate=False):
         del df_enriched["cl"]
     return df_enriched
 
-
-# In[95]:
-
-
+# TODO: Remove this hard-coded parameter
+threshold = 10
 resulting_voxels = np.argmax(ad_map.X, axis=1)
 if "F_out" in ad_map.obs.keys():
     filtered_voxels_to_types = [
@@ -475,15 +266,24 @@ if "F_out" in ad_map.obs.keys():
 
 else:
     vox_ct = [(resulting_voxels, adata_sc.obs['nowakowski_med'])]
+
+
 print(len(resulting_voxels))
 print(len(adata_sc.obs['nowakowski_med']))
+
 df_classes = one_hot_encoding(adata_sc.obs['nowakowski_med'])
+
 for index, i in enumerate(df_classes.columns):
     df_vox_cells[i] = 0
+
 colnames = list(df_vox_cells.columns)
+
+# TODO: This wasn't defined so we need to figure out what ind is or remove it
+ind = 1
 print(colnames)
 nan_ind = colnames.index(np.nan)
 print(ind)
+
 for i in range(0, len(resulting_voxels)):
     k = resulting_voxels[i]
     v = adata_sc.obs["nowakowski_med"][i]
@@ -495,10 +295,6 @@ for i in range(0, len(resulting_voxels)):
 
 ad.obsm["tangram_ct_count"] = df_vox_cells
 
-
-# In[ ]:
-
-
 tg.count_cell_annotations(
     adata_map = ad_map,
     adata_sc = adata_sc,
@@ -508,12 +304,5 @@ tg.count_cell_annotations(
 # nowakowski_med
 adata_st.obsm["tangram_ct_count"].head()
 
-
-# In[96]:
-
-
 adata_segment = tg.deconvolve_cell_annotations(ad)
 adata_segment.obs.head()
-
-
-# 
