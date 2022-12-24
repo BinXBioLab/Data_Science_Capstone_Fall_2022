4e0c02b4a03cf1f384fd54b8d043e3cf4cd2e8d7
Darvesh Gorhe <dgorhe123@gmail.com>
2022-12-24 15:52:43 -0500

    Tested Rscript separately and added output_path

--- optimized_codes/R/run_scran.R
+++ optimized_codes/R/run_scran.R
@@ -2,27 +2,41 @@ library(scran)
 library(tidyverse)
 library(Matrix)
 
-# Check that we have the correct number of arguments
+print("Parsing command line arguments")
 args <- commandArgs(trailingOnly = TRUE)
+format <- "Format: Rscript --vanilla input_file.mtx output_file.mtx"
 
 if (length(args) == 0) {
     stop(
-        "No arguments. Format: Rscript --vanilla counts metadata prefix",
+        paste("No arguments.", format, sep = " "),
         call. = FALSE
     )
-} else if (length(args) > 1) {
+} else if (length(args) == 1) {
     stop(
-        "Too many arguments. Format: Rscript --vanilla counts metadata prefix",
+        paste("Too few arguments.", format, sep = " "),
+        call. = FALSE
+    )
+} else if (length(args) > 2) {
+    stop(
+        paste("Too many.", format, sep = " "),
         call. = FALSE
     )
 }
 
-mat <- Matrix::readMM("optimized_codes/scripts/test.mtx")
+# Store input/output file paths
+input_path <- args[1]
+output_path <- args[2]
+
+print("Creating matrix and clustering")
+mat <- Matrix::readMM(input_path)
 cl <- quickCluster(mat)
 
+print("Computing sum factors")
 mysce <- computeSumFactors(
     SingleCellExperiment(list(counts = mat)),
     clusters = cl
 )
 
+print("Write array of double values to disk")
 sf <- sizeFactors(mysce)
+write.table(sf, file = output_path, col.names = FALSE, row.names = FALSE)
